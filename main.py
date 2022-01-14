ee5a057d59aaaa1b6f4ceac0b56f8052ce6ce716
rumezaa <97740994+rumezaa@users.noreply.github.com>
2022-01-14 12:06:13 -0700

    DO NOT ADD FILES

---
+++ main.py
@@ -0,0 +1,72 @@
+from tabulate import tabulate as table
+
+your_data = []
+
+
+choice = input("Open previous data?\n**If no previous data is found you will be directed to a new chart**\n (yes/no): ")
+if choice == "no":
+    mode = "w"
+else:
+    mode = "a"
+
+
+
+#append data into list
+
+def open_data():
+    with open("file_1.txt",mode="r") as file:
+        open_list = file.readlines()
+        for i,v in enumerate(open_list):
+            open_list=v.split()
+            your_data.append(open_list)
+
+def make_data():
+    global mode
+    create_list = input("")
+    create_list = f"{create_list}\n"
+
+    with open("file_1.txt", mode=mode) as file:
+        file.writelines(create_list)
+        mode= "a"
+
+
+if choice:
+    heads = input("Input the Categories for your chart: ")
+    header = heads.split()
+    rows = int(input('How Many Rows (5,10,15): '))
+
+    for i in range(rows):
+        make_data()
+        open_data()
+    print(table(your_data, header, "grid"))
+
+
+
+input("Press enter to exit")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
